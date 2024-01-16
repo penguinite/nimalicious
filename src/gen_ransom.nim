542a401315837c9f90f9f8aebeb7a3c0456890fc
penguinite <penguinite@tuta.io>
2024-01-16 13:45:12 +0100

    feat: Implement consent writing

--- src/gen_ransom.nim
+++ src/gen_ransom.nim
@@ -3,5 +3,11 @@
 
 import nimalicious/consent
 
-for i in prettyPrint("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a interdum purus. Nam viverra congue erat, in cursus nulla placerat ac. Cras ullamcorper consequat dui, non condimentum enim efficitur ut. Aliquam quis massa velit. Morbi et quam rutrum, pharetra sapien et, lacinia orci. Praesent non urna a tortor interdum interdum quis non arcu. Donec non felis eu est accumsan aliquet. Pellentesque id neque non arcu finibus volutpat quis bibendum turpis. Suspendisse aliquam lobortis tortor id accumsan. Curabitur eleifend elit quis tortor gravida sagittis. Curabitur non interdum augue, eget semper orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris scelerisque venenatis nisi, vitae vehicula eros commodo nec. Pellentesque scelerisque dictum fringilla. Pellentesque sit amet leo posuere sapien volutpat tincidunt."):
-  echo i
+askConsent(
+  "gen_random",
+  ransomwareDangers
+)
+
+
+
+echo prettyPrint("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a interdum purus. Nam viverra congue erat, in cursus nulla placerat ac. Cras ullamcorper consequat dui, non condimentum enim efficitur ut. Aliquam quis massa velit. Morbi et quam rutrum, pharetra sapien et, lacinia orci. Praesent non urna a tortor interdum interdum quis non arcu. Donec non felis eu est accumsan aliquet. Pellentesque id neque non arcu finibus volutpat quis bibendum turpis. Suspendisse aliquam lobortis tortor id accumsan. Curabitur eleifend elit quis tortor gravida sagittis. Curabitur non interdum augue, eget semper orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris scelerisque venenatis nisi, vitae vehicula eros commodo nec. Pellentesque scelerisque dictum fringilla. Pellentesque sit amet leo posuere sapien volutpat tincidunt.")
--- src/nimalicious/consent.nim
+++ src/nimalicious/consent.nim
@@ -1,53 +1,72 @@
 
 ## MUST BE RAN FOR EVERY BINARY
 
-import std/strutils
+import std/[os, times]
+from strutils import `%`
 
-const message* = """
-The program you are trying to run is malware, that can do real harm to your machine.
-This project was started to write open-source malware in Nim so that antiviruses can better fine-tune what \"suspicious\" or \"malicious\" behavior means.
+const
+  consentPattern{.strdefine.} = "Yes, I consent."
+  message = "The program you are trying to run is malware, that can do real harm to your machine. This project was started to write open-source malware in Nim so that antiviruses can better fine-tune what \"suspicious\" or \"malicious\" behavior means. "
+  consent = "\nRunning this program on another person's computer or property without asking for their consent is illegal, even as a joke or a prank. If you consent to this program running on your machine then the program authors cannot be held liable or responsible for any damages whatsoever. Are you sure you wish to proceed? Knowing that this program may permanently damage your computer. "
 
-Furthermore, running this program on another person's computer or property without asking for permission is illegal. Even as a joke.
-Running this program may result in the following:
-$#
-"""
+  prettyPrintLimit{.intdefine.}: int = 80
+  ransomwareDangers* = @[
+      "Files on your machine will be made inaccessible (via encryption)",
+      "You face high risk of losing important and valuable data permanently",
+      "You face high risk of destroying your operating system"
+    ]
 
-const consent* = "If you consent to this program running on your machine then the program authors cannot be held liable or responsible for any damages whatsoever.\nAre you sure you wish to proceed? Knowing that this program may permanently damage your computer"
-
+  consentDirs = @[
+    getHomeDir() & "Documents", # Save to the user's Document folder.
+    getHomeDir() & "Downloads", # Save to the user's Download folder.
+    getHomeDir() & "Desktop", # Save to the user's Download folder.
+    getHomeDir() & ".consent", # Save to a special consent folder
+    getConfigDir(), # Save to the user's config folder
+    getTempDir(), # Save to a temporary directory.
+  ]
 
 proc prettyList*(list: seq[string]): string =
+  result.add("\n")
   for i in list:
     result.add("\t * " & i & "\n")
   return result
 
-const prettyPrintLimit{.intdefine.}: int = 80
-
 proc prettyPrint*(str: string): string =
-  var
-    i = 0
-    tmp = ""
+  var i = 0
   for ch in str:
     inc(i)
-    if i == prettyPrintLimit:
-      if ch != '\n': tmp.add(ch)
+    if ch == ' ' and i >= prettyPrintLimit:
+      result.add("\n")
       i = 0
-      result.add(tmp & "\n")
-      tmp = ""
       continue
-  
-  if tmp.len() > 0:
-    result.add(tmp)
-
+    result.add(ch)
   return result
 
 
-proc askConsent*(name: string, dangers: seq[string], msg: string = message, question: string = consent) =
-  echo prettyPrint(msg)
+proc askConsent*(name: string, dangers: seq[string]) =
+  proc print(str: string) = stdout.write(prettyPrint(str))
+  proc printList(dng: seq[string]) = stdout.write(prettyList(dng) & "\n")
+  print(message)
+  print("Running this program may result in the following:")
+  printList(dangers)
+  print(consent)
+  print("Type \"" & consentPattern & "\" to consent.")
+  stdout.write("\n> ")
   let input = readLine(stdin)
   
-  if input.toLower() != "y" or input.toLower() != "yes":
-    echo "Exiting..."
+  if input != consentPattern:
+    print "Exiting..."
     quit(1)
-  
 
+  print "Saving consent with time and date."
 
+  var contents = "\nThis user (more specifically, the user who has a home directory in $#) has consented and allowed the \"$#\" program to run on their machine willingly.\nThis consent has taken place in Date: \"$#\", Time: \"$#\"\n\nThe original message, prior to the user's consent is as follows:\n$#\n$#\n$#\n\nThe user has typed \"$#\" and thus consented to the malicious program running on their machine.\nAs explained, prior to the consent, all program authors are not liable for any damages nor are they\nresponsible for any damages if you consent to the program running on your machine." % [getHomeDir(), name, getDateStr(), $getTime(), prettyPrint(message), prettyList(dangers), prettyPrint(consent), consentPattern]
+
+  for dir in consentDirs:
+    if dirExists(dir):
+      createDir(dir)
+    try:
+      writeFile(dir & "/" & name & "_consent_form.txt", contents)
+    except:
+      print "Failed to write consent. Revoking consent and exiting quietly."
+      quit(1)
